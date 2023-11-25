fb738dcff5249ec8b90bd5bf386e4451c037e9a4
SALVATOR69 <100500038+SALVATOR69@users.noreply.github.com>
2023-11-25 13:41:45 +0100

    make it work

---
+++ js/math.js
@@ -0,0 +1,24 @@
+function toggleList1() {
+  var content = document.getElementById("listContent1");
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
+function toggleList2() {
+  var content = document.getElementById("listContent2");
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
+function toggleList3() {
+  var content = document.getElementById("listContent3");
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
